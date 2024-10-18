6645b1f135326c9bd813617515ef4e12360bdcd0
Wilkes Booth <andersonwilkesbooth@gmail.com>
2024-10-18 13:02:08 -0400

    updated css bg

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import logo from ".//images/cgrc-logo.png";
-import email from ".//images/mail-outline.svg";
+import Logo from ".//images/cgrc-logo.png";
+import Email from ".//images/mail-outline.svg";
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
         <Image
           className='max-w-2xl'
-          src={logo}
+          src={Logo}
           alt='CGRC logo'
           width={0}
           height={0}
@@ -16,23 +16,8 @@ export default function Home() {
           style={{ width: "100%", height: "auto" }}
           priority
         />
-        {/* <a
-          className='flex items-center justify-items-center gap-2 hover:underline hover:underline-offset-4 m-auto'
-          href='mailto:bastian.bartels@contextgrc.com'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src={email}
-            alt='Window icon'
-            width={16}
-            height={16}
-          />
-          Reach Out For More Information
-        </a> */}
       </main>
-      <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
+      <footer className='row-start-3 flex flex-wrap items-center justify-center'>
         <a
           className='flex items-center gap-2 hover:underline hover:underline-offset-4'
           href='mailto:bastian.bartels@contextgrc.com'
@@ -41,7 +26,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={email}
+            src={Email}
             alt='Window icon'
             width={16}
             height={16}
